925bee2aa0c25147addfa37e268d178103193b82
Mariano Carrazana <marianocarrazana@gmail.com>
2023-01-09 00:32:45 -0300

    Update ruby version Add link helper

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -2,5 +2,6 @@ class ArticleController < ApplicationController
   def index
     art = Article.find(params[:id])
     @year = art.id
+    @month = art.created_at
   end
 end
--- app/helpers/article_helper.rb
+++ app/helpers/article_helper.rb
@@ -1,2 +1,6 @@
 module ArticleHelper
+  def gen_link(article)
+    created = article.created_at
+    "/article/#{created.year}/#{created.month}/#{created.day}/slug/#{article.id}"
+  end
 end
