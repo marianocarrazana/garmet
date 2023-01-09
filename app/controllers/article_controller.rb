e190270e3f08042f11940bfe0a4c6d299dbc8dab
Mariano Carrazana <marianocarrazana@gmail.com>
2023-01-09 01:32:40 -0300

    Add article edit

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -1,7 +1,11 @@
 class ArticleController < ApplicationController
   def index
-    art = Article.find(params[:id])
-    @year = art.id
-    @month = art.created_at
+    @art = Article.find(params[:id])
+    @year = @art.id
+    @month = @art.created_at
+  end
+
+  def edit
+    @art = Article.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'article/:year/:month/:day/:slug/:id', to: 'article#index', as: 'article'
+  get 'article/:year/:month/:day/:slug/:id', to: 'article#index'
+  get 'article/edit/:id', to: 'article#edit', as: 'art_edit'
   get 'dashboard', to: 'dashboard#index'
   get 'home', to: 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
